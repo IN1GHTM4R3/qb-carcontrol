e58e674108d3aa6926c46909ce4d6dc855bcc732
N1GHTM4R3 <84954945+IN1GHTM4R3@users.noreply.github.com>
2021-12-11 19:23:57 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,10 @@ fx_version 'adamant'
 
 game 'gta5'
 
+author 'N1GHTM4R3#0000'
+description 'qb-carcontrol for QBCore Created By N1GHTM4R3'
+version '1.0.1'
+
 client_scripts {
 	'client/main.lua',
 }
